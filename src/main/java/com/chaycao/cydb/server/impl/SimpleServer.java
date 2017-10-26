d1eebcf5beb6b37fb0932c5904c68252e0f13760
chaycao <429629038@qq.com>
2017-10-26 23:31:14 +0800

    初始化时读配置文件，创建相应的dataSource

--- src/main/java/com/chaycao/cydb/server/impl/SimpleServer.java
+++ src/main/java/com/chaycao/cydb/server/impl/SimpleServer.java
@@ -2,12 +2,14 @@ package com.chaycao.cydb.server.impl;
 
 import com.chaycao.cydb.dataSource.DataSource;
 import com.chaycao.cydb.dataSource.impl.SimpleDataSource;
-import com.chaycao.cydb.processor.impl.HttpRequestProcessor;
+import com.chaycao.cydb.processor.impl.SimpleRequestProcessor;
 import com.chaycao.cydb.server.AbstractServer;
 
+import java.io.File;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.ResourceBundle;
 
 /**
  * Created by chaycao on 2017/10/21.
@@ -19,27 +21,16 @@ public class SimpleServer extends AbstractServer implements Runnable {
     private ServerSocket server;
     private int numThreads = 50; //处理进程数
     private DataSource source;
+    private int numDataSources;
+
 
     /**
-     * 指定端口，不加载数据
+     * 指定端口
      * @param port
      */
     public SimpleServer(int port) {
-        this(port, null);
-    }
-
-    /**
-     * 指定端口，加载数据
-     * @param port
-     * @param dataFile
-     */
-    public SimpleServer(int port, String dataFile) {
         try {
             this.server = new ServerSocket(port);
-            if (dataFile == null)
-                this.source = new SimpleDataSource();
-            else
-                this.source = new SimpleDataSource(dataFile);
         } catch (IOException e) {
             System.out.println("端口被占用！请尝试别的端口");
             e.printStackTrace();
@@ -56,7 +47,7 @@ public class SimpleServer extends AbstractServer implements Runnable {
             try {
                 Socket request = server.accept();
                 // 调用HttpRequestProcessor处理
-                HttpRequestProcessor.processRequest(request);
+                SimpleRequestProcessor.processRequest(request);
             } catch (IOException e) {
                 e.printStackTrace();
             }
@@ -67,11 +58,26 @@ public class SimpleServer extends AbstractServer implements Runnable {
      * 初始化
      */
     public void initialize() {
+        // 读取dataSourceNum，若文件不存在，则生成
+        // 0号数据库作为初始数据库
+        ResourceBundle r = ResourceBundle.getBundle("server");
+        String num = r.getString("numDataSources");
+        String basePath = this.getClass().getClassLoader().getResource(".").getPath();
+        if (num == "")
+            this.numDataSources = 8; // 默认8个
+        else {
+            this.numDataSources = Integer.valueOf(num);
+        }
+        this.source = new SimpleDataSource(0);
+        for (int i = 1; i < numDataSources; i++) {
+            SimpleDataSource dataSource = new SimpleDataSource(i);
+        }
         // 处理进程初始化
         for (int i = 0; i < numThreads; i++) {
-            Thread t = new Thread(new HttpRequestProcessor(source));
+            Thread t = new Thread(new SimpleRequestProcessor(source));
             t.start();
         }
+
     }
 
     public static void main(String[] args) {
