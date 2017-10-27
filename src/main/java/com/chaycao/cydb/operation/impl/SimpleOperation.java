49b8eaece4cf89d6eb996e968477cc254c94df57
chaycao <429629038@qq.com>
2017-10-27 20:10:42 +0800

    添加select、exit方法

--- src/main/java/com/chaycao/cydb/operation/impl/SimpleOperation.java
+++ src/main/java/com/chaycao/cydb/operation/impl/SimpleOperation.java
@@ -2,24 +2,29 @@ package com.chaycao.cydb.operation.impl;
 
 import com.chaycao.cydb.command.impl.Get;
 import com.chaycao.cydb.command.impl.Save;
+import com.chaycao.cydb.command.impl.Select;
 import com.chaycao.cydb.command.impl.Set;
 import com.chaycao.cydb.dataSource.DataSource;
+import com.chaycao.cydb.processor.Processor;
+import com.chaycao.cydb.processor.impl.SimpleRequestProcessor;
+import com.chaycao.cydb.server.Server;
 
 /**
  * Created by chaycao on 2017/10/25.
  * chaycao@163.com
  *
- * 分析指令
- * get key --- 返回value
- * put key value
+ * 一次操作
+ * 负责根据指令，操作数据或对数据源做操作
  */
 public class SimpleOperation {
     private String command;
     private DataSource source;
+    private Processor processor;
 
-    public SimpleOperation(String command, DataSource source) {
+    public SimpleOperation(String command, DataSource source, Processor processor) {
         this.command = command;
         this.source = source;
+        this.processor = processor;
     }
 
     public String execute() {
@@ -37,6 +42,10 @@ public class SimpleOperation {
         if (c.equals("save")) {
             Save save = new Save(source);
             return save.exe();
+        }
+        if (c.equals("select")) {
+            Select select = new Select(processor);
+            return select.exe(args[1]);
         }
         return "system error";
     }
